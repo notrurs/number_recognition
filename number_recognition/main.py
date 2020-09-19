a20d14f80f0648b1dd8f49883dc9210a957a872c
notrurs <fursik111@gmail.com>
2020-09-19 09:52:10 +0300

    Got mnist dataset

---
+++ number_recognition/main.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
+
+
+def get_dataset():
+    train = datasets.MNIST('', train=True, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor()
+                           ]))
+
+    test = datasets.MNIST('', train=False, download=True,
+                          transform=transforms.Compose([
+                              transforms.ToTensor()
+                          ]))
+
+    trainset = DataLoader(train, batch_size=10, shuffle=True)
+    testset = DataLoader(test, batch_size=10, shuffle=False)
+
+    return trainset, testset
+
+
+def main():
+    trainset, testset = get_dataset()
+
+
+if __name__ == '__main__':
+    main()
